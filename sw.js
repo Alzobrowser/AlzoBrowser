1a3c578dd8f9b286ec27a9ae5b6ab5e5996c86e0
Alzobrowser <garrapachuno@gmail.com>
2025-08-19 11:04:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", event => {
+  console.log("Service Worker instalado");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      new Response("⚠️ No tienes conexión. Vuelve a intentarlo.")
+    )
+  );
+});
