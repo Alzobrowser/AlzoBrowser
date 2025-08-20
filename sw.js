75857ae3670468cd5751af6add934a141ad11ddc
Alzobrowser <garrapachuno@gmail.com>
2025-08-20 09:46:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,42 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("app-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
+// Nombre de la caché
+const CACHE_NAME = "amazon-buscador-cache-v1";
+
+// Archivos a cachear (ajusta si agregas más en el futuro)
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Instalar Service Worker y cachear recursos
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// Activar y limpiar cachés antiguas
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Interceptar peticiones y servir desde caché si es posible
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
